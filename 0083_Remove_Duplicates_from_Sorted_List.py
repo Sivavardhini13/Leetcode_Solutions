124dd97ee85da8fa346457405ead3b7100e8a874
Sivavardhini <85095079+Sivavardhini13@users.noreply.github.com>
2026-09-02 23:45:50 +0530

    Add files via upload

---
+++ 0083_Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,12 @@
+# Leetcode 83 : Remove Duplicates from Sorted List
+# Difficulty : Easy
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr=head
+        while curr and curr.next:
+            if curr.val==curr.next.val:
+                curr.next=curr.next.next
+            else:
+                curr=curr.next
+        return head
