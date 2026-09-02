93597a2e8fa79535c2fc231515ea480b2a2ac738
Sivavardhini <85095079+Sivavardhini13@users.noreply.github.com>
2026-09-03 00:40:53 +0530

    optimized solution
    Refactored merge function to concatenate and sort arrays.

--- 0088_Merge_Sorted_Array.py
+++ 0088_Merge_Sorted_Array.py
@@ -3,19 +3,7 @@
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        k=len(nums1)-1
-        i=m-1
-        j=n-1
-        while i>=0 and j>=0:
-            v1, v2=nums1[i], nums2[j]
-            if v1>v2:
-                nums1[k]=v1
-                i-=1
-            else:
-                nums1[k]=v2
-                j-=1
-            k-=1
-        while j>=0:
-           nums1[k]=nums2[j] 
-           j-=1
-           k-=1
+        ls=nums1[:m]+nums2[:n]
+        nums1[:]=ls
+        nums1.sort()
+        return nums1
