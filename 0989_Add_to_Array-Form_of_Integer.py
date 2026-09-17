86f240e211465e21f2826af65d4eec6cf6c98ce2
Sivavardhini <85095079+Sivavardhini13@users.noreply.github.com>
2026-09-17 13:23:34 +0530

    Add files via upload

---
+++ 0989_Add_to_Array-Form_of_Integer.py
@@ -0,0 +1,13 @@
+# Leetcode 989 : Add to Array-Form of Integer
+# Difficulty : Easy
+
+class Solution:
+    def addToArrayForm(self, num: list[int], k: int) -> list[int]:
+        for i in reversed(range(len(num))):
+            k, num[i] = divmod(num[i] + k, 10)
+
+        while k > 0:
+            num = [k % 10] + num
+            k //= 10
+        
+        return num
