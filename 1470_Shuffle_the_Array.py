be7215465368f02d432f0b6d6ad4fa61cadb7295
Sivavardhini <85095079+Sivavardhini13@users.noreply.github.com>
2026-09-25 22:36:53 +0530

    Add files via upload

---
+++ 1470_Shuffle_the_Array.py
@@ -0,0 +1,10 @@
+# Leetcode 1470 : Shuffle the Array
+# Difficulty : Easy
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans = []
+        for i in range(n):
+            ans.append(nums[i])
+            ans.append(nums[i+n])
+        return ans
